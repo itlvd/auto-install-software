92f2277a7e7de3e44081162a1af8ece915c6eeda
Le Van Dong <itlvd@outlook.com>
2022-09-10 16:46:46 +0700

    New project

---
+++ run.bat
@@ -0,0 +1,51 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
+
+cecho {CF}Downloading the Software from server file{#}{\n}
+wget.exe -N -P Downloaded --content-disposition -i server.txt -q --show-progress
+
+
+cecho {CF}Silent Installing{#}{\n}
+
+for %%a in (Downloaded\*.msi) do (
+echo Installing %%a
+msiexec /i %~dp0%%a /qn
+cecho {0A}    Successfull{#}{\n}
+)
+
+cecho {CF}Manual Installing{#}{\n}
+for %%a in (Downloaded\*.exe) do (
+echo Installing %%a
+%~dp0%%a
+cecho {0A}    Successfull{#}{\n})
+
+cecho {9F} Done {#}{\n}
+pause
